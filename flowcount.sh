b8085d3b2feb767f1efb80eb92671d43f91dc4f5
Konstantin Blagov <kblagov@cisco.com>
2015-06-23 14:26:05 +0200

    Fixed flowcount output
    (now substracts 1 from "wc -l" result to exclude header)

--- flowcount.sh
+++ flowcount.sh
@@ -10,16 +10,16 @@ then
         echo;echo "FLOWS:";ovs-ofctl dump-flows $sw -OOpenFlow13 table=$1 --rsort=priority
 	echo
 	printf "Flow count: "
-	ovs-ofctl dump-flows $sw -OOpenFlow13 table=$1 | wc -l
+	echo $(($(ovs-ofctl dump-flows $sw -OOpenFlow13 table=$1 | wc -l)-1))
 else
         printf "No table entered. $sw flow count: ";
-        ovs-ofctl dump-flows $sw -OOpenFlow13 | wc -l
-        printf "\nTable0: PortSecurity:  "; ovs-ofctl dump-flows $sw -OOpenFlow13 table=0| wc -l
-        printf "\nTable1: IngressNat:    "; ovs-ofctl dump-flows $sw -OOpenFlow13 table=1| wc -l
-        printf "\nTable2: SourceMapper:  "; ovs-ofctl dump-flows $sw -OOpenFlow13 table=2| wc -l
-        printf "\nTable3: DestMapper:    "; ovs-ofctl dump-flows $sw -OOpenFlow13 table=3| wc -l
-        printf "\nTable4: PolicyEnforcer:"; ovs-ofctl dump-flows $sw -OOpenFlow13 table=4| wc -l
-        printf "\nTable5: EgressNAT:     "; ovs-ofctl dump-flows $sw -OOpenFlow13 table=5| wc -l
-        printf "\nTable6: External:      "; ovs-ofctl dump-flows $sw -OOpenFlow13 table=6| wc -l
+        echo $(($(ovs-ofctl dump-flows $sw -OOpenFlow13 | wc -l)-1))
+        printf "\nTable0: PortSecurity:  "; echo $(($(ovs-ofctl dump-flows $sw -OOpenFlow13 table=0| wc -l)-1))
+        printf "\nTable1: IngressNat:    "; echo $(($(ovs-ofctl dump-flows $sw -OOpenFlow13 table=1| wc -l)-1))
+        printf "\nTable2: SourceMapper:  "; echo $(($(ovs-ofctl dump-flows $sw -OOpenFlow13 table=2| wc -l)-1))
+        printf "\nTable3: DestMapper:    "; echo $(($(ovs-ofctl dump-flows $sw -OOpenFlow13 table=3| wc -l)-1))
+        printf "\nTable4: PolicyEnforcer:"; echo $(($(ovs-ofctl dump-flows $sw -OOpenFlow13 table=4| wc -l)-1))
+        printf "\nTable5: EgressNAT:     "; echo $(($(ovs-ofctl dump-flows $sw -OOpenFlow13 table=5| wc -l)-1))
+        printf "\nTable6: External:      "; echo $(($(ovs-ofctl dump-flows $sw -OOpenFlow13 table=6| wc -l)-1))
 fi
 
