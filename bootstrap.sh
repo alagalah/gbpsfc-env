a7b7cd2a8b2700f2c378d815de82fefe237f807e
Tomas Cechvala <tcechval@cisco.com>
2015-09-23 15:24:59 +0200

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# vim: sw=4 ts=4 sts=4 et tw=72 :
+odl=$1
+echo "---> Updating operating system"
+apt-get update -qq
+apt-get upgrade -y --force-yes -qq
+
+echo "---> Installing Group Based Policy requirements"
+apt-get install -y software-properties-common -qq
+apt-get install -y python-software-properties -qq
+apt-get install -y python-pip -qq
+apt-get install -y git-core git -qq
+apt-get install -y curl -qq
+# docker
+curl -sSL https://get.docker.com/ | sh
+docker pull alagalah/odlpoc_ovs230
+# OVS
+curl https://raw.githubusercontent.com/pritesh/ovs/nsh-v8/third-party/start-ovs-deb.sh | bash
+
+# this part is just for local spinup DON'T copy it to releng bootstrap.sh
+pip install ipaddr
+echo "export PATH=$PATH:/vagrant" >> /home/vagrant/.profile
+echo "export ODL="$1 >> /home/vagrant/.profile
+usermod -aG docker vagrant
+
