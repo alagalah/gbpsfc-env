b0441f54ec821b32c94271241be465c84d2a208e
Keith Burns (alagalah) <alagalah@gmail.com>
2015-09-28 18:52:09 -0700

    Changed docker network to be in line with ODL CSIT
    Signed-off-by: Keith Burns (alagalah) <alagalah@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,21 @@
 # vim: sw=4 ts=4 sts=4 et tw=72 :
 
 echo "---> Updating operating system"
-apt-get update -qq
-apt-get upgrade -y --force-yes -qq
+apt update -qq
 
 echo "---> Installing Group Based Policy requirements"
-apt-get install -y software-properties-common -qq
-apt-get install -y python-software-properties -qq
-apt-get install -y python-pip -qq
-apt-get install -y git-core git -qq
-apt-get install -y curl -qq
+apt install -y software-properties-common -qq
+apt install -y python-software-properties -qq
+apt install -y python-pip -qq
+apt install -y git-core git -qq
+apt install -y curl -qq
 # docker
 curl -sSL https://get.docker.com/ | sh
+
+cat <<EOL > /etc/default/docker
+  DOCKER_NETWORK_OPTIONS='--bip=10.250.0.254/24'
+EOL
+
 docker pull alagalah/odlpoc_ovs230
 # OVS
 curl https://raw.githubusercontent.com/pritesh/ovs/nsh-v8/third-party/start-ovs-deb.sh | bash
