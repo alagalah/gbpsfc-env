24421e02d37484bcc5cf5dd2390ef7f68028b9f9
Keith Burns (alagalah) <alagalah@gmail.com>
2015-09-28 19:34:59 -0700

    Modified SF flowmods to account for dynamic NSP.
    Just decrements the NSI.
    
    Signed-off-by: Keith Burns (alagalah) <alagalah@gmail.com>

--- utils/sf-flows.sh
+++ utils/sf-flows.sh
@@ -16,17 +16,5 @@ fi
 # delete NORMAL, if present
 nsphex=`printf "%x\n" $nsp`
 sudo ovs-ofctl --strict del-flows $sw priority=0
-sudo ovs-ofctl add-flow $sw "priority=1000,nsp=$nsp,nsi=255 actions=move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:$TUNNEL->NXM_NX_TUN_IPV4_DST[],set_nsi:254,set_nsp:0x$nsphex,IN_PORT" -OOpenFlow13
-sudo ovs-ofctl add-flow $sw "priority=1000,nsp=$nsp,nsi=254 actions=move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:$TUNNEL->NXM_NX_TUN_IPV4_DST[],set_nsi:253,set_nsp:0x$nsphex,IN_PORT" -OOpenFlow13
-
-let "nsp += 1"
-nsphex=`printf "%x\n" $nsp`
-sudo ovs-ofctl add-flow $sw "priority=1000,nsp=$nsp,nsi=255 actions=move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:$TUNNEL->NXM_NX_TUN_IPV4_DST[],set_nsi:254,set_nsp:0x$nsphex,IN_PORT" -OOpenFlow13
-sudo ovs-ofctl add-flow $sw "priority=1000,nsp=$nsp,nsi=254 actions=move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:$TUNNEL->NXM_NX_TUN_IPV4_DST[],set_nsi:253,set_nsp:0x$nsphex,IN_PORT" -OOpenFlow13
-
-#sudo ovs-ofctl add-flow $sw "priority=1000,nsp=$nsp,nsi=255 actions=move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:0xC0A83247->NXM_NX_TUN_IPV4_DST[],set_nsi:254,set_nsp:$nsp,IN_PORT" -OOpenFlow13
-#sudo ovs-ofctl add-flow $sw "priority=1000,nsp=$nsp,nsi=254 actions=move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:0xC0A83249->NXM_NX_TUN_IPV4_DST[],set_nsi:253,set_nsp:$nsp,IN_PORT" -OOpenFlow13
-
-#let "nsp += 1"
-#sudo ovs-ofctl add-flow $sw "priority=1000,nsp=$nsp,nsi=255 actions=move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:0xC0A83249->NXM_NX_TUN_IPV4_DST[],set_nsi:254,set_nsp:$nsp,IN_PORT" -OOpenFlow13
-#sudo ovs-ofctl add-flow $sw "priority=1000,nsp=$nsp,nsi=254 actions=move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:0xC0A83247->NXM_NX_TUN_IPV4_DST[],set_nsi:253,set_nsp:$nsp,IN_PORT" -OOpenFlow13
+sudo ovs-ofctl add-flow $sw "priority=1000,nsi=255 actions=move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:$TUNNEL->NXM_NX_TUN_IPV4_DST[],set_nsi:254,IN_PORT" -OOpenFlow13
+sudo ovs-ofctl add-flow $sw "priority=1000,nsi=254 actions=move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:$TUNNEL->NXM_NX_TUN_IPV4_DST[],set_nsi:253,IN_PORT" -OOpenFlow13
