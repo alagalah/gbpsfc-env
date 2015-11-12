4c8ffd3e28546d97deb75f51c030dd44a8008264
Keith Burns (alagalah) <alagalah@gmail.com>
2015-11-12 11:24:12 -0800

    Changed to sf-dpl-name and sff-dpl-name in rest.py for SFCOFL2 changes
    Signed-off-by: Keith Burns (alagalah) <alagalah@gmail.com>

--- demo-symmetric-chain/rest.py
+++ demo-symmetric-chain/rest.py
@@ -116,9 +116,8 @@ def get_service_function_forwarders_data():
                         "name": "firewall-72",
                         "type": "service-function-type:firewall",
                         "sff-sf-data-plane-locator": {
-                            "port": 6633,
-                            "ip": "192.168.50.71",
-                             "transport": "service-locator:vxlan-gpe"
+                            "sf-dpl-name": "2",
+			    "sff-dpl-name": "sfc-tun2"
                         }
                     }
                 ],
@@ -155,9 +154,8 @@ def get_service_function_forwarders_data():
                         "name": "dpi-74",
                         "type": "service-function-type:dpi",
                         "sff-sf-data-plane-locator": {
-                            "port": 6633,
-                            "ip": "192.168.50.73",
-                             "transport": "service-locator:vxlan-gpe"
+			    "sf-dpl-name": "3",
+                            "sff-dpl-name": "sfc-tun4"
                         }
                     }
                 ],
