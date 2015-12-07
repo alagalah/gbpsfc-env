f09161388b19716bcdcd731ffc8a159f16f02dcc
Keith Burns (alagalah) <alagalah@gmail.com>
2015-12-07 09:43:11 -0800

    Moving demo env to OpenDaylight (See README)
    Signed-off-by: Keith Burns (alagalah) <alagalah@gmail.com>

--- demo-gbp1/get-nsps.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-if __name__ == "__main__":
-    # Launch main menu
-
-
-    # Some sensible defaults
-    print "Nothing"
--- env.sh
+++
@@ -1,3 +0,0 @@
-export NUM_NODES=6
-export ODL="192.168.50.1"
-export SUBNET="192.168.50."
